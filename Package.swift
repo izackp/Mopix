514e8232a0a3c7dcdafba67c0ebe84f29bf0c409
Isaac Paul <izackp@gmail.com>
2023-11-16 14:21:36 -0500

    better

--- Package.swift
+++ Package.swift
@@ -23,10 +23,11 @@ let package = Package(
     ],
     dependencies: [
         //.package(path: "/Users/isaacpaul/Projects/swift-projects/SDL"),
+        //.package(path: "/Users/isaacpaul/Projects/swift-projects/icu-swift"),
         .package(url: "https://github.com/izackp/SDL.git", branch: "master"),
         .package(url: "https://github.com/eonil/FSEvents.git", from:"0.1.7"),
         .package(url: "https://github.com/izackp/icu-swift.git", branch: "master"),
-        .package(url: "https://github.com/t-ae/xorswift", from: "3.0.0")
+        .package(url: "https://github.com/t-ae/xorswift", from: "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
